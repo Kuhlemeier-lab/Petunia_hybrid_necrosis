70a35963749736c12bcee5be69746d72f90eca57
Marta Binaghi <25846389+martabe@users.noreply.github.com>
2022-08-30 17:31:09 +0200

    update genome and removed unnecessary filters

--- code/il07_filter_variants.sh
+++ code/il07_filter_variants.sh
@@ -14,8 +14,7 @@
 
 # Partition
 #SBATCH --partition=epyc2
-##SBATCH --qos=job_epyc2_debug
-#SBATCH --account=ips_ck
+#SBATCH --account=xxx
 #SBATCH --chdir=/xxx/necrosis
 #SBATCH --output=code/il07_filter_variants_%A.out
 #SBATCH --error=code/il07_filter_variants_%A.err
@@ -40,7 +39,7 @@ echo -e "#### Filter variants for quality and select
 ###\t####\n
 "
 
-genome=${scdir}/data/genomes/Peax402INV.fasta
+genome=${scdir}/data/genomes/Peax403.fasta
 
 # Filter variants
 echo -e "## Hard filter SNP variants 
@@ -114,105 +113,3 @@ picard-tools MergeVcfs \
 
 echo -e "Hard filter, select, merge SNP and INDEL done
 "
-
-# Extract variants associated to phenotype
-
-echo -e "
-# Selection of variants following the phenotype.
-"
-
-# the sample name (in the quotes after getGenotype) corresponds to names given in script il04.
-# Here we select as if first sample, kmh1 which is necrotic is homozygous for the alternate allele,
-GenomeAnalysisTK SelectVariants \
-   -R ${genome} \
-   --restrict-alleles-to BIALLELIC \
-   -V ${scdir}/data/raw/il_variants/clean.vcf \
-   --selectExpressions 'vc.getGenotype("kmh1").isHomVar()' \
-   --selectExpressions 'vc.getGenotype("kmh2").isHomVar()' \
-   --selectExpressions 'vc.getGenotype("kmh3").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh4").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh5").isHomVar()' \
-   --selectExpressions 'vc.getGenotype("kmh6").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh7").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh8").isHomVar()' \
-   --selectExpressions 'vc.getGenotype("kmh9").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh10").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh11").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh12").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh13").isHomRef()' \
-   --selectExpressions 'vc.getGenotype("kmh14").isHomRef()' \
-   -O ${scdir}/data/raw/il_variants/clean_phenotype1_necroticIsAlternate.vcf
-
-# Now we select as if kmh1 (necrotic) is homozygous reference.
-GenomeAnalysisTK SelectVariants \
-    -R ${genome} \
-    --restrict-alleles-to BIALLELIC \
-    -V ${scdir}/data/raw/il_variants/clean.vcf \
-    --selectExpressions 'vc.getGenotype("kmh1").isHomRef()' \
-    --selectExpressions 'vc.getGenotype("kmh2").isHomRef()' \
-    --selectExpressions 'vc.getGenotype("kmh3").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh4").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh5").isHomRef()' \
-    --selectExpressions 'vc.getGenotype("kmh6").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh7").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh8").isHomRef()' \
-    --selectExpressions 'vc.getGenotype("kmh9").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh10").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh11").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh12").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh13").isHomVar()' \
-    --selectExpressions 'vc.getGenotype("kmh14").isHomVar()' \
-    -O ${scdir}/data/raw/il_variants/clean_phenotype2_necroticIsReference.vcf
-
-echo -e "Selection done."
-
-# Count variants in different steps and save them in summary.txt
-echo -e "
-# Summary of variant counts..."
-
-echo -e "####    Summary of variants
-## `date`
-" > ${scdir}/data/raw/il_variants/summary.txt
-
-# make and empty temporary file to store the summary values before formatting
-echo "" > ${scdir}/data/raw/il_variants/summary.tmp
-
-echo -e "FILE\tCOUNT" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -vc '^#' ${scdir}/data/raw/il_variants/raw.vcf)
-echo -e "raw.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -vc '^#' ${scdir}/data/raw/il_variants/raw_snp.vcf)
-echo -e "raw_snp.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -vc '^#' ${scdir}/data/raw/il_variants/raw_indel.vcf)
-echo -e "raw_indel.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -vc '^#' ${scdir}/data/raw/il_variants/selected_snp.vcf)
-echo -e "selected_snp.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -vc '^#' ${scdir}/data/raw/il_variants/selected_indel.vcf)
-echo -e "selected_indel.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -vc '^#' ${scdir}/data/raw/il_variants/clean.vcf)
-echo -e "clean.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -Pc '1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1.*1/1' ${scdir}/data/raw/il_variants/clean.vcf)
-echo -e "all samples homozygous alternate (1/1),${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -cvP "^#" ${scdir}/data/raw/il_variants/clean_phenotype1_necroticIsAlternate.vcf)
-echo -e "clean_phenotype1_necroticIsAlternate.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-# for the next step, we checked the order of the samples in the vcf file,
-# which is kmh: 1,10,11,12,13,14,2,3,4,5,6,7,8,9
-count=$(grep -cP '1/1.*0/0.*0/0.*0/0.*0/0.*0/0.*1/1.*0/0.*0/0.*1/1.*0/0.*0/0.*1/1.*0/0' ${scdir}/data/raw/il_variants/clean.vcf)
-echo -e "grep count on phenotype1,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -cPv "^#" ${scdir}/data/raw/il_variants/clean_phenotype2_necroticIsReference.vcf)
-echo -e "clean_phenotype2_necroticIsReference.vcf,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-count=$(grep -cP '0/0.*1/1.*1/1.*1/1.*1/1.*1/1.*0/0.*1/1.*1/1.*0/0.*1/1.*1/1.*0/0.*1/1' ${scdir}/data/raw/il_variants/clean.vcf)
-echo -e "grep count on phenotype2,${count}" >> ${scdir}/data/raw/il_variants/summary.tmp
-cat ${scdir}/data/raw/il_variants/summary.tmp | column -s "," -t >> ${scdir}/data/raw/il_variants/summary.txt  # format first part of the summary table
-echo -e "\n### Phenotype 1 Necrotis is alternate ###" >> ${scdir}/data/raw/il_variants/summary.txt
-echo -e "VARIANTS\tSCF" >> ${scdir}/data/raw/il_variants/summary.txt
-grep -oP '^(Chr|Scaffold_)\d+' ${scdir}/data/raw/il_variants/clean_phenotype1_necroticIsAlternate.vcf | sort | uniq -c >> ${scdir}/data/raw/il_variants/summary.txt
-echo -e "\n### Phenotype 2 Necrotic is reference ###" >> ${scdir}/data/raw/il_variants/summary.txt
-echo -e "VARIANTS\tSCF" >> ${scdir}/data/raw/il_variants/summary.txt
-grep -oP '^(Chr|Scaffold_)\d+' ${scdir}/data/raw/il_variants/clean_phenotype2_necroticIsReference.vcf | sort | uniq -c >> ${scdir}/data/raw/il_variants/summary.txt
-
-# remove summary.tmp
-rm ${scdir}/data/raw/il_variants/summary.tmp
-
-echo -e "Summary done. Results are stored in ${scdir}/data/raw/il_variants/summary.txt.
-
-End at `date`"
