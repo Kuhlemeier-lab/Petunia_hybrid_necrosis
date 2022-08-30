2de252a1e1b90aff19507a0180acdaa352c04127
Marta Binaghi <25846389+martabe@users.noreply.github.com>
2022-08-30 17:27:10 +0200

    update

--- code/il03_stats_index_genome.sh
+++ code/il03_stats_index_genome.sh
@@ -36,30 +36,30 @@ module add UHTS/Analysis/picard-tools/2.18.11
 module list 2>&1
 
 ## Raw reads quality statistics
-#echo -e "## Calculation of quality stats of raw reads\n"
-#cd ${scdir}/data/raw/il_rawreads
-#echo -e "Stats on raw reads\n`date`\n" > stats_raw_reads.txt
-#for file in *.fastq.gz  # for each raw file in the folder, calculates stats and store them in stats_raw_reads.txt
-#do
-#	echo -e "\n################################# \n $file" >> stats_raw_reads.txt
-#	fastq-stats $file >> stats_raw_reads.txt
-#done
-#echo -e "Quality stats done and stored in data/raw/il_rawreads/stats_raw_reads.txt.\n"
-## Clean reads quality statistics
-#echo -e "## Calculation of quality stats of trimmed reads\n"
-#cd ${scdir}/data/raw/il_rawreads
-#echo -e "Stats on trimmed reads\n`date`\n" > stats_trimmed_reads.txt
-#for file in *.fastq.gz.trimmed  # for each trimmed file in the folder, calculates stats and store them in stats_trimmed_reads.txt
-#do
-#	echo -e "\n################################# \n $file" >> stats_trimmed_reads.txt
-#	fastq-stats $file >> stats_trimmed_reads.txt
-#done
-#echo -e "Quality stats done and stored in data/raw/raw_reads/stats_trimmed_reads.txt.\n"
-#
-## Index genome
+echo -e "## Calculation of quality stats of raw reads\n"
+cd ${scdir}/data/raw/il_rawreads
+echo -e "Stats on raw reads\n`date`\n" > stats_raw_reads.txt
+for file in *.fastq.gz  # for each raw file in the folder, calculates stats and store them in stats_raw_reads.txt
+do
+	echo -e "\n################################# \n $file" >> stats_raw_reads.txt
+	fastq-stats $file >> stats_raw_reads.txt
+done
+echo -e "Quality stats done and stored in data/raw/il_rawreads/stats_raw_reads.txt.\n"
+# Clean reads quality statistics
+echo -e "## Calculation of quality stats of trimmed reads\n"
+cd ${scdir}/data/raw/il_rawreads
+echo -e "Stats on trimmed reads\n`date`\n" > stats_trimmed_reads.txt
+for file in *.fastq.gz.trimmed  # for each trimmed file in the folder, calculates stats and store them in stats_trimmed_reads.txt
+do
+	echo -e "\n################################# \n $file" >> stats_trimmed_reads.txt
+	fastq-stats $file >> stats_trimmed_reads.txt
+done
+echo -e "Quality stats done and stored in data/raw/raw_reads/stats_trimmed_reads.txt.\n"
+
+# Index genome
 genome=${scdir}/data/genomes/Peax403.fasta
-#
-#bwa index ${genome}
-#samtools faidx ${genome}
+
+bwa index ${genome}
+samtools faidx ${genome}
 picard-tools CreateSequenceDictionary R=${genome} O=${genome}.dict
 echo -e "Indexing done.\n"
